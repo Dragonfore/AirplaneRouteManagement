263fdf788f61cee2cdc9ddad461a4be3dfeebf49
Matthew Mackay <matthew.mackay@centralsquare.com>
2023-11-29 13:57:51 -0800

    Update icon and add total cities UI.

--- AirplaneRouteManagement/Form1.Designer.cs
+++ AirplaneRouteManagement/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TotalCitiesLabel = new System.Windows.Forms.Label();
+            this.TotalCitiesValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TotalCitiesLabel
+            // 
+            this.TotalCitiesLabel.AutoSize = true;
+            this.TotalCitiesLabel.Location = new System.Drawing.Point(13, 13);
+            this.TotalCitiesLabel.Name = "TotalCitiesLabel";
+            this.TotalCitiesLabel.Size = new System.Drawing.Size(62, 13);
+            this.TotalCitiesLabel.TabIndex = 0;
+            this.TotalCitiesLabel.Text = "Total Cities:";
+            // 
+            // TotalCitiesValue
+            // 
+            this.TotalCitiesValue.AutoSize = true;
+            this.TotalCitiesValue.Location = new System.Drawing.Point(72, 13);
+            this.TotalCitiesValue.Name = "TotalCitiesValue";
+            this.TotalCitiesValue.Size = new System.Drawing.Size(13, 13);
+            this.TotalCitiesValue.TabIndex = 1;
+            this.TotalCitiesValue.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1063, 619);
+            this.Controls.Add(this.TotalCitiesValue);
+            this.Controls.Add(this.TotalCitiesLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Route Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TotalCitiesLabel;
+        private System.Windows.Forms.Label TotalCitiesValue;
     }
 }
 
